8c2d1ac8448e6a7c3993eb9b32af47849a77a2b0
Vladimir Vrzić <vvrzic@gmail.com>
2025-11-04 15:14:28 +0100

    Extract output formatting functionality

---
+++ constants.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	StatRSS          = "rss"
+	StatPSS          = "pss"
+	StatPrivateClean = "private_clean"
+	StatPrivateDirty = "private_dirty"
+)
+
+const (
+	ExitSuccess          = 0
+	ExitInvalidArguments = 1
+)
